ce420c43e792512a3fb03b74e8d1e60d25c5b4d3
Marvin Große Besselmann <grosse@fzi.de>
2023-06-05 09:14:56 +0200

    Adjusted CMakeList file to support client library function
    Signed-off-by: Marvin Große Besselmann <grosse@fzi.de>

---
+++ sick_safevisionary_baseConfig.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}" ${CMAKE_MODULE_PATH})
+include(CMakeFindDependencyMacro)
+
+include("${CMAKE_CURRENT_LIST_DIR}/sick_safevisionary_baseTargets.cmake")
