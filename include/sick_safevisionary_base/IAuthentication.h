d4854adfcc612f4d2e86f0fbd411152178bdbd00
Marvin Große Besselmann <grosse@fzi.de>
2023-08-08 14:24:55 +0200

    Updated license
    Signed-off-by: Marvin Große Besselmann <grosse@fzi.de>

--- include/sick_safevisionary_base/IAuthentication.h
+++ include/sick_safevisionary_base/IAuthentication.h
@@ -1,10 +1,23 @@
-/// \copyright Redistribution and use in source, with or without modification, are permitted.
-///
-/// Definition of the interface used for authentication at a SICK SOPAS device
-///
-/// \email TechSupport0905@sick.de
-///
-/// \version 0.0.1
+// -- BEGIN LICENSE BLOCK ----------------------------------------------
+/*!
+*  Copyright (C) 2023, SICK AG, Waldkirch, Germany
+*  Copyright (C) 2023, FZI Forschungszentrum Informatik, Karlsruhe, Germany
+*
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+
+*/
+// -- END LICENSE BLOCK ------------------------------------------------
 
 #pragma once
 #include <string>
--- src/UdpSocket.cpp
+++ src/UdpSocket.cpp
@@ -1,10 +1,23 @@
-//
-// Copyright note: Redistribution and use in source, with or without modification, are permitted.
-//
-// Created: January 2020
-//
-// SICK AG, Waldkirch
-// email: TechSupport0905@sick.de
+// -- BEGIN LICENSE BLOCK ----------------------------------------------
+/*!
+*  Copyright (C) 2023, SICK AG, Waldkirch, Germany
+*  Copyright (C) 2023, FZI Forschungszentrum Informatik, Karlsruhe, Germany
+*
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+
+*/
+// -- END LICENSE BLOCK ------------------------------------------------
 
 #include <cstring>
 
